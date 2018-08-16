a6bbc1671453cfc05dfd02ddfbfbae70123f6058
Lucas <lfg.machado@hotmail.com>
2018-08-16 16:55:09 -0300

    Initial commit.

---
+++ BookStoreApi/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o título do livro.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do autor.")]
+        public string AuthorName { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome da editora.")]
+        public string Publisher { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double Preco { get; set; }
+    }
+}
